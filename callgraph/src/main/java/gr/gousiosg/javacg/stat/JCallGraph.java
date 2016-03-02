12b7fe4f178ff2153d189809aa7691acca656535
walter alves <walter.alves@ccc.ufcg.edu.br>
2016-03-02 15:51:52 -0300

    Code organized2

--- callgraph/src/main/java/gr/gousiosg/javacg/stat/JCallGraph.java
+++ callgraph/src/main/java/gr/gousiosg/javacg/stat/JCallGraph.java
@@ -41,75 +41,73 @@ import org.apache.bcel.classfile.ClassParser;
 import y.view.Graph2D;
 import y.view.Graph2DView;
 
-
-
 public class JCallGraph {
 
-	
 	private String jarName;
 	private String pattern;
-	  Graph2DView view;
-	  Graph2D graph;
+	Graph2DView view;
+	Graph2D graph;
 
 	public JCallGraph(String jarName, String pattern) {
 		this.jarName = jarName;
 		this.pattern = pattern;
-		
+
 	}
 
-//	public static void main(String[] args) {
-//        ClassParser cp;
-//        if(args.length >= 3){
-//        	File f = new File(args[0]);
-//        	 if (!f.exists()) {
-//                 System.err.println("Jar file " + args[0] + " does not exist");
-//                 System.exit(-1);
-//             }
-//        	
-//        	 JarFile jar = null;
-//			try {
-//				jar = new JarFile(f);
-//			} catch (IOException e1) {
-//				 System.err.println("Error while processing jar: " + e1.getMessage());
-//				 e1.printStackTrace();
-//				 System.exit(-1);
-//			}
-//
-//			Utils.deleteFiles(args[2]);
-//			
-//             Enumeration<JarEntry> entries = jar.entries();
-//             while (entries.hasMoreElements()) {
-//                 JarEntry entry = entries.nextElement();
-//                 if (entry.isDirectory())
-//                     continue;
-//
-//                 if (!entry.getName().endsWith(".class"))
-//                     continue;
-//
-//                 cp = new ClassParser(args[0],entry.getName());
-//                 ClassVisitor visitor;
-//				try {
-//					visitor = new ClassVisitor(cp.parse(),args[1];
-//					visitor.start();
-//				} catch (ClassFormatException e) {
-//					 System.err.println("Error while processing jar: " + e.getMessage());
-//					 e.printStackTrace();
-//				} catch (IOException e) {
-//					 System.err.println("Error while processing jar: " + e.getMessage());
-//			         e.printStackTrace();
-//				}
-//             }
-//         
-//        	 
-//        	
-//        }else{
-//        	System.err.println("Params error. You have to execute \"java -jar projectToAnalyze.jar pattern outputFilePrefix\"");
-//        	System.exit(-1);
-//        	
-//        }
-//        
-//        
-//    }
+	// public static void main(String[] args) {
+	// ClassParser cp;
+	// if(args.length >= 3){
+	// File f = new File(args[0]);
+	// if (!f.exists()) {
+	// System.err.println("Jar file " + args[0] + " does not exist");
+	// System.exit(-1);
+	// }
+	//
+	// JarFile jar = null;
+	// try {
+	// jar = new JarFile(f);
+	// } catch (IOException e1) {
+	// System.err.println("Error while processing jar: " + e1.getMessage());
+	// e1.printStackTrace();
+	// System.exit(-1);
+	// }
+	//
+	// Utils.deleteFiles(args[2]);
+	//
+	// Enumeration<JarEntry> entries = jar.entries();
+	// while (entries.hasMoreElements()) {
+	// JarEntry entry = entries.nextElement();
+	// if (entry.isDirectory())
+	// continue;
+	//
+	// if (!entry.getName().endsWith(".class"))
+	// continue;
+	//
+	// cp = new ClassParser(args[0],entry.getName());
+	// ClassVisitor visitor;
+	// try {
+	// visitor = new ClassVisitor(cp.parse(),args[1];
+	// visitor.start();
+	// } catch (ClassFormatException e) {
+	// System.err.println("Error while processing jar: " + e.getMessage());
+	// e.printStackTrace();
+	// } catch (IOException e) {
+	// System.err.println("Error while processing jar: " + e.getMessage());
+	// e.printStackTrace();
+	// }
+	// }
+	//
+	//
+	//
+	// }else{
+	// System.err.println("Params error. You have to execute \"java -jar
+	// projectToAnalyze.jar pattern outputFilePrefix\"");
+	// System.exit(-1);
+	//
+	// }
+	//
+	//
+	// }
 
 	public void prepare() {
 		ClassParser cp;
@@ -128,51 +126,48 @@ public class JCallGraph {
 			e1.printStackTrace();
 			System.exit(-1);
 		}
-		
+
 		Enumeration<JarEntry> entries = jar.entries();
-        while (entries.hasMoreElements()) {
-            JarEntry entry = entries.nextElement();
-            if (entry.isDirectory())
-                continue;
+		while (entries.hasMoreElements()) {
+			JarEntry entry = entries.nextElement();
+			if (entry.isDirectory())
+				continue;
 
-            if (!entry.getName().endsWith(".class"))
-                continue;
+			if (!entry.getName().endsWith(".class"))
+				continue;
 
-            cp = new ClassParser(this.jarName,entry.getName());
-            ClassVisitor visitor;
+			cp = new ClassParser(this.jarName, entry.getName());
+			ClassVisitor visitor;
 			try {
-				visitor = new ClassVisitor(cp.parse(),this.pattern);
+				visitor = new ClassVisitor(cp.parse(), this.pattern);
 				visitor.start();
 			} catch (ClassFormatException e) {
-				 System.err.println("Error while processing jar: " + e.getMessage());
-				 e.printStackTrace();
+				System.err.println("Error while processing jar: " + e.getMessage());
+				e.printStackTrace();
 			} catch (IOException e) {
-				 System.err.println("Error while processing jar: " + e.getMessage());
-		         e.printStackTrace();
+				System.err.println("Error while processing jar: " + e.getMessage());
+				e.printStackTrace();
 			}
-        }
+		}
 
 	}
-	
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		JCallGraph cg = new JCallGraph(args[0], args[1]);
 		cg.prepare();
-		for(String line : ClassVisitor.edges){
-			if(Pattern.compile("([a-z][a-z_0-9]*\\.)*[A-Z_]($[A-Z_]|[\\w_])*:[\\w_]* ([a-z][a-z_0-9]*\\.)*[A-Z_]($[A-Z_]|[\\w_])*:[\\w_]*").matcher(line).find()){
+		for (String line : ClassVisitor.edges) {
+			if (Pattern
+					.compile(
+							"([a-z][a-z_0-9]*\\.)*[A-Z_]($[A-Z_]|[\\w_])*:[\\w_]* ([a-z][a-z_0-9]*\\.)*[A-Z_]($[A-Z_]|[\\w_])*:[\\w_]*")
+					.matcher(line).find()) {
 				System.out.println(line + "[method-to-method]");
-				String[] nodesDeVided = line.split(" ");				
-				
-			}else{
+				String[] nodesDeVided = line.split(" ");
+
+			} else {
 				System.out.println(line + "[class-to-class]");
 			}
 		}
-		
+
 	}
 
-	
-
-	
-	
-	
 }
